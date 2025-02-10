bb7a6336821af4fbe279da5dc6e47d99f7e984dd
tadeo-dev789 <tadeosandovalsa@gmail.com>
2025-02-10 13:44:48 -0600

    Modificaciones para el dia 10-02-2025

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.ejemplo1
 
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,26 +25,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                    Content()
-                }
+                GreetingPreview()
             }
         }
     }
-
+}
 
 @Preview(showBackground = true)
 @Composable
-fun Content(){
-    Text(
-        text = "Hola Jetpack Compose",
-        color = Color.Red,
-        textAlign = TextAlign.Center,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp
-    )
-    /**Modificadores
-     * De posicicionamiento = width, height, top, buttom, start, end
-     * De funcionalidad = click, scroll
-     * De apariencia = background, padding, border
-     */
+fun GreetingPreview(){
+    Content("Hola","Mundo")
+}
+
+@Composable
+fun Content(mensaje1:String, mensaje2:String){
+    Column{
+        Text(mensaje1, fontSize = 30.sp)
+        Text(mensaje2, lineHeight = 30.sp)
+        Text("Dante", fontWeight = FontWeight.Bold, fontSize = 30.sp, lineHeight = 32.sp)
+    }
 }
