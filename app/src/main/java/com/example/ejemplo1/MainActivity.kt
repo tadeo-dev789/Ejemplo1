d6ccb5ed9611922a462570101293427227ce21aa
tadeo-dev789 <tadeosandovalsa@gmail.com>
2025-02-10 13:40:56 -0600

    Modificaciones para el dia 06-02-2025

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
@@ -20,26 +21,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                    Content()
+                    GrettingPreview()
                 }
             }
         }
     }
 
 
+
 @Preview(showBackground = true)
 @Composable
-fun Content(){
-    Text(
-        text = "Hola Jetpack Compose",
-        color = Color.Red,
-        textAlign = TextAlign.Center,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp
-    )
-    /**Modificadores
-     * De posicicionamiento = width, height, top, buttom, start, end
-     * De funcionalidad = click, scroll
-     * De apariencia = background, padding, border
-     */
+fun GrettingPreview(){
+    Content("Hola","Mundo")
+}
+
+
+@Composable
+fun Content(mensaje1:String,mensaje2:String){
+    Column{
+        Text(mensaje1, fontSize = 30.sp)
+        Text(mensaje2, lineHeight = 30.sp)
+        Text("Luis", fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            lineHeight = 32.sp)
+    }
 }
