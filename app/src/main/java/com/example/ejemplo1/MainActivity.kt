9a435ba7b46f2a36f656f099817ccb31f8afe861
tadeo-dev789 <tadeosandovalsa@gmail.com>
2025-02-17 13:21:43 -0600

    Cambios guardados el dia  17-02-2025

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,42 +4,183 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                    Content()
-                }
+                    GreetingPreview()
             }
         }
     }
+}
 
-
+@Composable
 @Preview(showBackground = true)
+fun GreetingPreview(){
+    Column {
+//        Content()
+//        Content2()
+//        Content3()
+//        Content4()
+//        Content5()
+//        ScaleModifier()
+//        BorderModifier()
+//        AlphaModifier()
+//        RotateModifier()
+//        WeightModifier()
+        ClipModifier()
+    }
+}
 @Composable
 fun Content(){
-    Text(
-        text = "Hola Jetpack Compose",
-        color = Color.Red,
-        textAlign = TextAlign.Center,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp
+    Text("LUIS ENRIQUE TADEO SANDOVAL SANTOS",
+        Modifier.padding(32.dp) // Outer padding (margin)
+            .background(color = Color.Green) //background color
+            .padding(16.dp) // Inner padding
     )
-    /**Modificadores
-     * De posicicionamiento = width, height, top, buttom, start, end
-     * De funcionalidad = click, scroll
-     * De apariencia = background, padding, border
-     */
 }
+
+@Composable
+fun Content2(){
+    Text("LUIS ENRIQUE TADEO SANDOVAL SANTOS",
+        Modifier.padding(32.dp) // Outer padding (margin)
+            .background(color = Color.Red) //background color
+            .padding(16.dp) // Inner padding
+    )
+}
+
+@Composable
+fun Content3(){
+    Text(
+        "LUIS ENRIQUE TADEO SANDOVAL SANTOS",
+        Modifier.padding(32.dp) // Outer padding (margin)
+            .background(color = Color.Green) //background color
+            .padding(16.dp) // Inner padding
+    )
+}
+
+@Composable
+fun Content4(){
+    Text(
+        text = "LUIS ENRIQUE TADEO SANDOVAL SANTOS",
+        color = Color.White,
+        modifier = Modifier
+            .background(Color.Blue)
+            .width(200.dp)
+            .height(300.dp)
+    )
+}
+
+@Composable
+fun Content5(){
+    Text(
+        text = "LUIS ENRIQUE TADEO SANDOVAL SANTOS",
+        color = Color.White,
+        modifier = Modifier
+            .background(Color.Gray)
+            .padding(Dp(10f))
+            .fillMaxWidth(1f))
+}
+
+@Composable
+fun AlphaModifier() {
+    Box(
+        Modifier
+            .size(250.dp)
+            .alpha(0.5f)//50% opacity
+            .background(Color.Red)
+    )
+}
+
+@Composable
+fun RotateModifier() {
+    Box(
+        Modifier
+            .rotate(45f)
+            .size(250.dp)
+            .background(Color.Red)
+    )
+}
+
+@Composable
+fun ScaleModifier() {
+    Box(
+        Modifier
+            .scale(scaleX = 2f, scaleY = 3f)
+            .size(200.dp, 200.dp)
+    )
+}
+
+
+@Composable
+fun WeightModifier(){
+    Row() {
+        Column(
+            Modifier.weight(1f).background(Color.Red)){
+            Text(text = "Weight = 1", color = Color.White)
+        }
+        Column(
+            Modifier.weight(1f).background(Color.Blue)){
+            Text(text = "Weight = 1", color = Color.White)
+        }
+        Column(
+            Modifier.weight(2f).background(Color.Green)
+        ) {
+            Text(text = "Weight = 2")
+        }
+
+    }
+}
+
+@Composable
+fun BorderModifier() {
+    Text(
+        text = "Text with Red Border",
+        modifier = Modifier
+            .padding(10.dp)
+            .background(Color.Yellow)
+            .border(2.dp,Color.Red)
+            .padding(10.dp)
+    )
+}
+
+@Composable
+fun ClipModifier() {
+    Text(
+        text = "Text with Clipped background",
+        color = Color.White,
+        modifier = Modifier
+            .padding(Dp(10f))
+            .clip(RoundedCornerShape(25.dp))
+            .background(Color.Blue)
+            .padding(Dp(15f))
+    )
+}
+
