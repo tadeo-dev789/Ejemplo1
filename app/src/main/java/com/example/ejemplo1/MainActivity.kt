09f7eec74b0290fd3e21c755d84872011b0aff9f
tadeo-dev789 <tadeosandovalsa@gmail.com>
2025-02-17 13:46:41 -0600

    Cambios guardados el dia  17-02-2025

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,23 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+
 
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                    Content()
+                    GreetingPreview()
                 }
             }
         }
@@ -29,17 +39,95 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun Content(){
-    Text(
-        text = "Hola Jetpack Compose",
-        color = Color.Red,
-        textAlign = TextAlign.Center,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp
-    )
-    /**Modificadores
-     * De posicicionamiento = width, height, top, buttom, start, end
-     * De funcionalidad = click, scroll
-     * De apariencia = background, padding, border
-     */
+fun GreetingPreview() {
+    SimpleImage()
+    CircleImageView()
+    RoundCornerImageView()
+    ImageWithBackgroundColor()
 }
+
+
+@Composable
+fun SimpleImage() {
+    Image(
+        painter = painterResource(id = R.drawable.andy_rubin),
+        contentDescription = "Andy Rubin",
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable fun CircleImageView() {
+    Image( painter = painterResource(R.drawable.andy_rubin),
+        contentDescription = "Circle Image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier .size(128.dp)
+            .clip(CircleShape)
+            .border(5.dp, Color.Gray, CircleShape) )
+}
+
+@Composable
+fun RoundCornerImageView() {
+    Image(
+        painter = painterResource(R.drawable.andy_rubin),
+        contentDescription = "Round corner image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(RoundedCornerShape(10))
+            .border(5.dp, Color.Gray, RoundedCornerShape(10))
+    )
+}
+
+
+@Composable
+fun ImageWithBackgroundColor() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_cart),
+        contentDescription = "",
+        modifier = Modifier
+            .size( 200.dp)
+            .background(Color.DarkGray)
+            .padding(20.dp)
+    )
+}
+
+
+@Composable
+fun ImageWithTint() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_cart),
+        contentDescription = "",
+        colorFilter = ColorFilter.tint(Color.Red),
+        modifier = Modifier
+            .size( 200.dp)
+    )
+}
+
+@Composable
+fun InsideFit() {
+    Image(
+        painter = painterResource(id = R.drawable.andy_rubin),
+        contentDescription = "",
+        modifier = Modifier
+            .size(150.dp)
+            .background(Color.LightGray),
+        contentScale = ContentScale.Inside
+    )
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun Content(){
+//    Text(
+//        text = "Hola Jetpack Compose",
+//        color = Color.Red,
+//        textAlign = TextAlign.Center,
+//        fontWeight = FontWeight.Bold,
+//        fontSize = 20.sp
+//    )
+//    /**Modificadores
+//     * De posicicionamiento = width, height, top, buttom, start, end
+//     * De funcionalidad = click, scroll
+//     * De apariencia = background, padding, border
+//     */
+//}
