6732860f43e07840515130bc9f21d2281539485c
tadeo-dev789 <tadeosandovalsa@gmail.com>
2025-02-27 14:59:22 -0600

    Cambios guardados el dia  26-02-2025

--- app/src/main/java/com/example/ejemplo1/navigation/NavManager.kt
+++ app/src/main/java/com/example/ejemplo1/navigation/NavManager.kt
@@ -1,16 +1,18 @@
 package com.example.ejemplo1.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.ejemplo1.views.HomeView
 
 @Composable
-fun NavManager(){
+fun NavManager() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "Home"){
-        composable(route:"Home"){
-    }
+
+    NavHost(navController = navController, startDestination = "Home") {
+        composable(route = "Home") {
+            HomeView()
+        }
     }
 }
