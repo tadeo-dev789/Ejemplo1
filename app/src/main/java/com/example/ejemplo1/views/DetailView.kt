199e5232a677f4063c01b8493ad5afb1299b4041
tadeo-dev789 <tadeosandovalsa@gmail.com>
2025-03-04 13:54:47 -0600

    Cambios guardados el dia  04-03-2025

--- app/src/main/java/com/example/ejemplo1/views/DetailView.kt
+++ app/src/main/java/com/example/ejemplo1/views/DetailView.kt
@@ -1,21 +1,27 @@
 package com.example.ejemplo1.views
 
 import android.annotation.SuppressLint
+import android.content.ClipData.Item
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.ejemplo1.components.ActionButton
 import com.example.ejemplo1.components.MainIconButton
@@ -53,15 +59,45 @@ fun DetailView(navController: NavController){
 
 @Composable
 private fun ContentView(navController: NavController){
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        TitleView("Hola")
-        Spacers()
-        MainButton("Generico",Color.Blue,Color.White){
-            navController.popBackStack()
+    LazyColumn {
+        item {
+            Text(
+                text = "Hola",
+                Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = "Hola",
+                Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = "Hola",
+                Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = "Hola",
+                Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = "Hola",
+                Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+        item {
+            Text(
+                text = "Hola",
+                Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = "Hola",
+                Modifier.padding(64.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
         }
     }
 }
